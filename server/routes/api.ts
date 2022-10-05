0ecb7200d8f27471334bb35f1d5951b99ecbe9cf
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-05 17:12:11 +0100

    created general structure for api routes

---
+++ server/routes/api.ts
@@ -0,0 +1,19 @@
+export const startGame = (
+  req: Express.Request,
+  res: Express.Response
+): void => {};
+
+export const submitAnswer = (
+  req: Express.Request,
+  res: Express.Response
+): void => {};
+
+export const getCurrentScore = (
+  req: Express.Request,
+  res: Express.Response
+): void => {};
+
+export const resetGame = (
+  req: Express.Request,
+  res: Express.Response
+): void => {};
