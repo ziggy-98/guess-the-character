6467422e6a56e49909a086da4d222dbc78f47313
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-05 15:47:30 +0100

    Fixed issues with start and test Changed start command to look at correct file Changed initial test to check that index route returns an empty object

--- server/__tests__/index.jest.ts
+++ server/__tests__/index.jest.ts
@@ -2,19 +2,19 @@ import app from "../index";
 import request from "supertest";
 import { convertCompilerOptionsFromJson } from "typescript";
 
-var server: any;
+let server: any;
 
 describe("Server startup", () => {
   beforeEach(() => {
     server = app;
   });
-  it("Server starts up on port 3000", (done) => {
+  it("Server starts up", (done) => {
     request(app)
       .get("/")
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res).toBeUndefined();
+        expect(res).toMatchObject({});
         done();
       });
   });
--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.json();
+});
+
 export default app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
