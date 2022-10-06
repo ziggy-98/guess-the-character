c299ad907875611ca0df9b4536be84188e48dd4d
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-06 12:57:40 +0100

    changed the jest preset to ts-jest Now jest files written in ts will be read properly

--- jest.server.config.js
+++ jest.server.config.js
@@ -3,6 +3,7 @@ module.exports = {
     displayName: "server",
     setupFilesAfterEnv: ["<rootDir>/jest-server-setup.js"],
     testMatch: ["<rootDir>/**/__tests__/*.jest.(ts|tsx)"],
+    preset: "ts-jest",
     testPathIgnorePatterns: ["<rootDir>/test/"],
   };
   
