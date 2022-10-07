e5148f944e795d03ad343400d43d688505d120a1
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-07 13:20:07 +0100

    updated index file Added nodeCache Added support for NodeCache to be populated with test data when testing Added routing middlewares Added nunjuck configuration

---
+++ server/src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import nunjucks from "nunjucks";
+import path from "path";
+import NodeCache from "node-cache";
+import routes from "./routes";
+import testUserData from "../__tests__/testUserData.json";
+
+export const app = express();
+export const cache = new NodeCache({ stdTTL: 86400 });
+if (process.env.NODE_ENV === "test") {
+  cache.mset(
+    (Object.keys(testUserData) as Array<keyof typeof testUserData>).reduce(
+      (acc, currentUserDataObj) => {
+        acc.push({
+          key: currentUserDataObj,
+          val: testUserData[currentUserDataObj],
+        });
+        return acc;
+      },
+      []
+    )
+  );
+}
+const port = 3000;
+
+nunjucks.configure(path.resolve(__dirname, "views"), {
+  express: app,
+  autoescape: true,
+  noCache: false,
+  watch: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", routes);
+
+app.get("/", (req, res) => {
+  res.render("index.html", {});
+});
+
+export default app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
