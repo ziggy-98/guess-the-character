9e82e3f5063a74d8d24a6b40d437cbe41b355c14
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-07 16:49:36 +0100

    Added functionality for handlers for game routes getCurrentScore resetGame submitAnswer

--- server/src/routes/handlers/api/getCurrentScore.ts
+++ server/src/routes/handlers/api/getCurrentScore.ts
@@ -1,3 +1,15 @@
 import { Request, Response } from "express";
+import { UserDataObject } from "../../../types";
+import { cache } from "../../../";
 
-export const getCurrentScore = (req: Request, res: Response): void => {};
+export const getCurrentScore = (req: Request, res: Response): void => {
+  if (req.query.uuid) {
+    const userData: UserDataObject | undefined = cache.get(
+      req.query.uuid as string
+    );
+    const score = userData ? userData.score : 0;
+    res.send(score.toString());
+  } else {
+    res.redirect("/error");
+  }
+};
--- server/src/routes/handlers/api/submitAnswer.ts
+++ server/src/routes/handlers/api/submitAnswer.ts
@@ -1,3 +1,23 @@
 import { Request, Response } from "express";
+import { UserDataObject } from "../../../types";
+import { cache } from "../../../index";
 
-export const submitAnswer = (req: Request, res: Response): void => {};
+export const submitAnswer = (req: Request, res: Response): void => {
+  if (req.body.round && req.body.answer && req.body.uuid) {
+    let oldData: UserDataObject | undefined = cache.get(req.body.uuid);
+    if (oldData) {
+      let correct = req.body.round.correctAnswer === req.body.answer;
+      let newData = {
+        ...oldData,
+        score: correct ? oldData.score + 1 : oldData.score,
+      };
+      cache.set(req.body.uuid, newData);
+      res.send({
+        userData: newData,
+        correct: correct,
+      });
+    }
+  } else {
+    res.redirect("/error");
+  }
+};
