d90e4a3547e971e22f2bf6211f3f2c873bbad7b1
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-08 15:29:10 +0100

    added view routes to router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,9 @@
 export * from "./api";
 import express from "express";
-import { apiRoutes } from "./api";
+import apiRoutes from "./api";
+import viewRoutes from "./views";
 
 const routes = express.Router();
 routes.use(apiRoutes);
+routes.use(viewRoutes);
 export default routes;
