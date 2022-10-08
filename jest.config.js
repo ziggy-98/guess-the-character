8c0591720ea85899383f9399f8bc3142429f7f79
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-08 19:38:56 +0100

    removed testing for client due to lack of time

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 module.exports = {
     projects: [
       {
-        roots: ["<rootDir>/client"],
-        displayName: "client",
-        setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
-        testMatch: ["<rootDir>/**/__tests__/*.jest.(ts|tsx)"],
-        testPathIgnorePatterns: ["<rootDir>/test/"],
-        moduleNameMapper: {
-          "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
-        },
-      },
-      {
         displayName: "server",
         roots: ["<rootDir>/server"],
         setupFilesAfterEnv: ["<rootDir>/jest-server-setup.js"],
