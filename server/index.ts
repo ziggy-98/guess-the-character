394cbb09e5ae09c44f1d36c2b66b717d8172707e
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-08 15:30:00 +0100

    moved index.ts file to root of server

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,16 @@
 import express from "express";
-import nunjucks from "nunjucks";
-import path from "path";
 import NodeCache from "node-cache";
-import routes from "./routes";
-import testUserData from "../__tests__/testUserData.json";
+import path from "path";
+import routes from "./src/routes";
 
 export const app = express();
 export const cache = new NodeCache({ stdTTL: 86400 });
 const port = 3000;
 
-nunjucks.configure(path.resolve(__dirname, "views"), {
-  express: app,
-  autoescape: true,
-  noCache: false,
-  watch: true,
-});
-
+app.use(express.static(path.resolve(__dirname, "client")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
-
-app.get("/", (req, res) => {
-  res.render("index.html", {});
-});
 
 export default app.listen(port, () => {
   console.log(`Server listening on ${port}`);
