8eaf7a70acdafc94324fc6f6b03155096ab3a3c7
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-09 11:53:37 +0100

    Added the api call for starting a game

--- client/src/api/user-data.ts
+++ client/src/api/user-data.ts
@@ -1,6 +1,19 @@
+import { UserGameResponse } from "server";
+
 export const UserDataApi = {
-  startGame: () => {},
+  startGame: (uuid: string = undefined): Promise<UserGameResponse | Error> => {
+    return new Promise((resolve, reject) => {
+      fetch("/api/start-game", {
+        method: "POST",
+        body: JSON.stringify({ uuid: uuid }),
+      })
+        .then((res) => res.json())
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+    });
+  },
   submitAnswer: () => {},
   resetGame: () => {},
   getCurrentScore: () => {},
+  getCurrentRound: () => {},
 };
