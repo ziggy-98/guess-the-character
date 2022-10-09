1f8c26d665ba6bb6fc56e1d7f84edd313b7dcdfc
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-10-09 17:04:05 +0100

    added apis calls on frontend submitAnswer api for submitting the user's answer getCurrentScore for getting the score of the current user

--- client/src/api/user-data.ts
+++ client/src/api/user-data.ts
@@ -1,4 +1,4 @@
-import { Round, UserGameResponse } from "server";
+import { Round, UserGameResponse, AnswerResponse, RoundResponse } from "server";
 
 export const UserDataApi = {
   startGame: (uuid: string = undefined): Promise<UserGameResponse | Error> => {
@@ -16,7 +16,29 @@ export const UserDataApi = {
         .catch((err) => reject(err));
     });
   },
-  submitAnswer: () => {},
+  submitAnswer: (
+    uuid: string,
+    round: Round,
+    answer: string
+  ): Promise<AnswerResponse | Error> => {
+    return new Promise((resolve, reject) => {
+      fetch("/api/submit-answer", {
+        method: "PATCH",
+        body: JSON.stringify({
+          uuid: uuid,
+          round: round,
+          answer: answer,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+    });
+  },
   resetGame: (uuid: string = undefined): Promise<UserGameResponse | Error> => {
     return new Promise((resolve, reject) => {
       fetch("/api/reset", {
@@ -32,8 +54,15 @@ export const UserDataApi = {
         .catch((err) => reject(err));
     });
   },
-  getCurrentScore: () => {},
-  getCurrentRound: (uuid: string): Promise<Round | Error> => {
+  getCurrentScore: (uuid: string): Promise<number | Error> => {
+    return new Promise((resolve, reject) => {
+      fetch(`/api/score?uuid=${uuid}`)
+        .then((res) => res.text())
+        .then((res) => resolve(parseInt(res)))
+        .catch((err) => reject(err));
+    });
+  },
+  getCurrentRound: (uuid: string): Promise<RoundResponse | Error> => {
     return new Promise((resolve, reject) => {
       fetch(`/api/round?uuid=${uuid ?? ""}`)
         .then((res) => res.json())
